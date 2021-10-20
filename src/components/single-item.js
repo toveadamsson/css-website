7a43b9a84e4c1fc0d4c7d9d031b2205499bcbc38
Tove Adamsson <toveadamsson@gmail.com>
2021-10-20 11:53:13 +0200

    updated pictures

--- src/components/single-item.js
+++ src/components/single-item.js
@@ -1,11 +1,19 @@
 import React from "react";
-import bigLeaf from "../images/green-leaves-nature-background-wallpaper.jpg";
+import PictureOne from "../images/IMG_1075.jpeg";
+import PictureTwo from "../images/IMG_1091.jpeg";
+import PictureThree from "../images/IMG_1144.jpeg";
+import PictureFour from "../images/IMG_3069.jpeg";
+import PictureFive from "../images/IMG_2181.jpeg";
+import PictureSix from "../images/IMG_0559.jpeg";
 function SingleItem() {
   return (
     <div className="different-selection">
       <div className="individual-container">
         <div className="image-container">
-          <img src={bigLeaf} alt="big leaf"></img>
+          <img
+            src={PictureOne}
+            alt="nature landscape kayaking sunrise sunset"
+          ></img>
         </div>
         <h1>Aenean aliquet sapien</h1>
         <p>
@@ -18,8 +26,8 @@ function SingleItem() {
       <div className="individual-container">
         <div className="image-container">
           <img
-            src="https://www.hdnicewallpapers.com/Walls/Big/Nature%20and%20Landscape/Beautiful_Nature_Background_Wallpapers.jpg"
-            alt="big leaf"
+            src={PictureTwo}
+            alt="nature landscape kayaking sunrise sunset"
           ></img>
         </div>
         <h1>Mauris in odio vel odio</h1>
@@ -32,34 +40,9 @@ function SingleItem() {
       <div className="individual-container">
         <div className="image-container">
           <img
-            src="https://images.pexels.com/photos/38537/woodland-road-falling-leaf-natural-38537.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="big leaf"
+            src={PictureThree}
+            alt="nature landscape kayaking sunrise sunset"
           ></img>{" "}
-        </div>
-        <h1>Sagittis sodales enim</h1>
-        <p>
-          You are allowed to use this video catalog for your business websites.
-          Maecenas bibendum, ex in convallis accumsan, mi dolor accumsan diam,
-          sed faucibus lorem sed
-        </p>
-      </div>
-      <div className="individual-container">
-        <div className="image-container">
-        <img src={bigLeaf} alt="big leaf"></img>
-        </div>
-        <h1>Sagittis sodales enim</h1>
-        <p>
-          You are allowed to use this video catalog for your business websites.
-          Maecenas bibendum, ex in convallis accumsan, mi dolor accumsan diam,
-          sed faucibus lorem sed
-        </p>
-      </div>
-      <div className="individual-container">
-        <div className="image-container">
-        <img
-            src="https://www.hdnicewallpapers.com/Walls/Big/Nature%20and%20Landscape/Beautiful_Nature_Background_Wallpapers.jpg"
-            alt="big leaf"
-          ></img>
         </div>
         <h1>Sagittis sodales enim</h1>
         <p>
@@ -71,9 +54,38 @@ function SingleItem() {
       <div className="individual-container">
         <div className="image-container">
           <img
-            src="https://images.pexels.com/photos/38537/woodland-road-falling-leaf-natural-38537.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="big leaf"
-          ></img>{" "}
+            src={PictureFour}
+            alt="nature landscape kayaking sunrise sunset"
+          ></img>
+        </div>
+        <h1>Aenean aliquet sapien</h1>
+        <p>
+          Video thumbnail has a link to another HTML page. Categories do not
+          need any JS. They are just separated HTML pages. Paging is at the
+          bottom to extend the list as long as you want. This can be a large
+          catalog.
+        </p>
+      </div>
+      <div className="individual-container">
+        <div className="image-container">
+          <img
+            src={PictureFive}
+            alt="nature landscape kayaking sunrise sunset"
+          ></img>
+        </div>
+        <h1>Mauris in odio vel odio</h1>
+        <p>
+          You may need TemplateMo for a quick chat or send an email if you have
+          any question about this CSS template. font-family: 'Source Sans Pro',
+          sans-serif; for this template.
+        </p>
+      </div>
+      <div className="individual-container">
+        <div className="image-container">
+          <img
+            src={PictureSix}
+            alt="nature landscape kayaking sunrise sunset"
+          ></img>
         </div>
         <h1>Sagittis sodales enim</h1>
         <p>
@@ -82,8 +94,6 @@ function SingleItem() {
           sed faucibus lorem sed
         </p>
       </div>
-      
-     
     </div>
   );
 }
